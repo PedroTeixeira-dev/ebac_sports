1dab17ec782502549db6b5c92ece375eb495e7f7
Pedro Teixeira <pedro15at@gmail.com>
2024-01-11 11:14:11 -0300

    finalizando o exercicio

---
+++ src/store/reducers/favoritos.ts
@@ -0,0 +1,33 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Produto } from '../../App'
+
+type FavoritoState = {
+  itens: Produto[]
+}
+
+const initialState: FavoritoState = {
+  itens: []
+}
+
+const favoritoSlice = createSlice({
+  name: 'favoritos',
+  initialState,
+
+  reducers: {
+    favoritar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+
+      if (state.itens.find((p) => p.id === produto.id)) {
+        const favoritoSemProduto = state.itens.filter(
+          (p) => p.id !== produto.id
+        )
+        state.itens = favoritoSemProduto
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { favoritar } = favoritoSlice.actions
+export default favoritoSlice.reducer
